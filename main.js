469eb88e4d4c4a82978f9e14c1ffc10189ae6487
Renato Souza <renatoosoouzaa26@gmail.com>
2022-02-15 15:53:14 -0300

    teste metodo filter

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 let teste = require("./db")
 function pikachu(pokemon) {
-    let nome = pokemon.name
-    let tipo = pokemon.types[0]
-    let habilidade = pokemon.ability
-    return habilidade
+    let a = pokemon.filter(function (b) {
+        let random = b
+        return random
+    })
+    return a
 }
 
 console.log(pikachu(teste))
+// let nome = pokemon.name
+// let tipo = pokemon.types[0]
+// let habilidade = pokemon.ability
