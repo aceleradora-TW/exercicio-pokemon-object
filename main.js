2ce334113c9bb3037111ee54319a18659353833e
Renato Souza <renatoosoouzaa26@gmail.com>
2022-02-16 11:52:37 -0300

    implementa console.log na função pikachu

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 let teste = require("./db")
 function pikachu(pokemon) {
-    let a = pokemon.filter(function (b) {
-        let random = b
-        return random
-    })
-    return a
+    console.log("nome: " + pokemon.name + "  " + " tipo: " + pokemon.types[0])
+    console.log("habilidade: " + pokemon.ability)
+    console.log("")
+    console.log("Linha de Evolução:")
+    console.log(pokemon.preEvolution + " > " + pokemon.name.toUpperCase() + " > " + pokemon.evolution)
+    return
 }
 
 console.log(pikachu(teste))
-// let nome = pokemon.name
-// let tipo = pokemon.types[0]
-// let habilidade = pokemon.ability
