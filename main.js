b35c3edcecb4b8075ad606d57146fcd356bcafd2
duda sadino <dudababy27@gmail.com>
2022-02-16 13:57:33 -0300

    teste do metodo for no exercicio

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
+const pokemon = require("./db");
 let teste = require("./db")
 function pikachu(pokemon) {
-    console.log("nome: " + pokemon.name + "  " + " tipo: " + pokemon.types[0])
-    console.log("habilidade: " + pokemon.ability)
-    console.log("")
-    console.log("Linha de Evolução:")
-    console.log(pokemon.preEvolution + " > " + pokemon.name.toUpperCase() + " > " + pokemon.evolution)
-    return
-}
 
+    for (let i = 0; i != pokemon.lenght; i++)
+        result[i] = f(pokemon[i]);
+
+    return result
+}
+console.log(pikachu(pokemon).lenght())
 console.log(pikachu(teste))
+
+// console.log("nome: " + pokemon.name + "  " + " tipo: " + pokemon.types[0])
+// console.log("habilidade: " + pokemon.ability)
+// console.log("")
+// console.log("Linha de Evolução:")
+// console.log(pokemon.preEvolution + " > " + pokemon.name.toUpperCase() + " > " + pokemon.evolution)
+
