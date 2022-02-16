ccb153cf9a8c2eff50b99aa91915a41ac962c021
duda sadino <dudababy27@gmail.com>
2022-02-16 15:55:34 -0300

    alteração no for da função pikachu

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
-const pokemon = require("./db");
 let teste = require("./db")
 function pikachu(pokemon) {
 
     for (let i = 0; i != pokemon.lenght; i++)
-        result[i] = f(pokemon[i]);
+        // result[i] = f(pokemon[i]);
 
-    return result
+        return i
 }
-console.log(pikachu(pokemon).lenght())
-console.log(pikachu(teste))
+// console.log(pikachu(pokemon).lenght())
+console.log(teste.lenght)
 
 // console.log("nome: " + pokemon.name + "  " + " tipo: " + pokemon.types[0])
 // console.log("habilidade: " + pokemon.ability)
