efeefb784900adf0612c3e82ad41162e32833da7
duda sadino <dudababy27@gmail.com>
2022-02-21 15:54:17 -0300

    testando o metodo state no exercicio

--- main.js
+++ main.js
@@ -8,9 +8,25 @@ function pikachu(pokemon) {
 Linha de Evolução:
 ${pokemon.preEvolution} >> ${pokemon.name.toUpperCase()} >> ${pokemon.evolution}`)
 }
+
+
+function setState(state) {
+    return pokemonInfo[state.pokemonInfo, dataTwoArray]
+}
+
+let pokemon = "nome"
+let habilidade = "habilidade"
+let pokeforca = "força"
+
+function getElement(pokemon) {
+    return Array.from(pokemon)
+}
+
+
+
 // console.log(pikachu(pokemon).lenght())
 console.log(pikachu(teste))
-
+console.log(getElement(teste))
 // console.log("nome: " + pokemon.name + "  " + " tipo: " + pokemon.types[0])
 // console.log("habilidade: " + pokemon.ability)
 // console.log("")
