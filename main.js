a971f7cc7e53bae1388b8b98d696a82246a7951e
Renato Souza <renatoosoouzaa26@gmail.com>
2022-02-22 15:55:31 -0300

    complementa ataques da função

--- main.js
+++ main.js
@@ -1,14 +1,33 @@
 let teste = require("./db")
 function pikachu(pokemon) {
+    let ataques = pokemon.moves.sort(function (a, b) {
+        return a.lv - b.lv
+    })
+    console.log(ataques)
 
+    return (`Nome: ${(pokemon.name).charAt(0).toUpperCase() + (pokemon.name).slice(1)} - Tipo: ${(pokemon.types[0]).charAt(0).toUpperCase() + (pokemon.types[0]).slice(1)}\nHabilidade: ${pokemon.ability.charAt(0).toUpperCase() + (pokemon.ability).slice(1)}\n
+    Linha de Evolução:
+    ${pokemon.preEvolution.charAt(0).toUpperCase() + (pokemon.preEvolution).slice(1)} >> ${pokemon.name.toUpperCase()} >> ${pokemon.evolution.charAt(0).toUpperCase() + (pokemon.evolution).slice(1)}\n
+    Atributos:\n
+    HP: ${pokemon.attributes.hp}
+    ATK: ${pokemon.attributes.attack} SpATK: ${pokemon.attributes.specialAttack}
+    DEF: ${pokemon.attributes.defense} SpDEF: ${pokemon.attributes.specialDefense}
+    SPEED: ${pokemon.attributes.speed}\n
+    Ataques:
+    Lv ${ataques[0].lv} - ${ataques[0].name}
+    Lv ${ataques[1].lv} - ${ataques[1].name}
+    Lv ${ataques[2].lv} - ${ataques[2].name}
+    Lv ${ataques[3].lv} - ${ataques[3].name}`
 
-
+    )
 }
-
-
+// Lv 5 -  ${pokemon.moves[0].name.charAt(0).toUpperCase() + (pokemon.moves[0].name).slice(1)}
+// Lv 9 -  ${pokemon.moves[3].name.charAt(0).toUpperCase() + (pokemon.moves[3].name).slice(1)}
+// Lv 20 - ${pokemon.moves[1].name.charAt(0).toUpperCase() + (pokemon.moves[1].name).slice(1)}
+// Lv 50 -  ${pokemon.moves[2].name.charAt(0).toUpperCase() + (pokemon.moves[2].name).slice(1)})
 // console.log(pikachu(pokemon).lenght())
 console.log(pikachu(teste))
-
+// console.log(Object.entries(teste))
 
 // console.log("nome: " + pokemon.name + "  " + " tipo: " + pokemon.types[0])
 // console.log("habilidade: " + pokemon.ability)
@@ -27,18 +46,3 @@ console.log(pikachu(teste))
 // let nome = pokemon.name
 // let tipo = pokemon.types[0]
 // let habilidade = pokemon.ability
-//return string.charAt(0).toUpperCase() + string.slice(1);
-
-// console.log(`Nome: ${(teste.name).charAt(0).toUpperCase() + (teste.name).slice(1)} - Tipo: ${(teste.types[0]).charAt(0).toUpperCase() + (teste.types[0]).slice(1)}\nHabilidade: ${teste.ability.charAt(0).toUpperCase() + (teste.ability).slice(1)}\n
-// Linha de Evolução:
-// ${teste.preEvolution.charAt(0).toUpperCase() + (teste.preEvolution).slice(1)} >> ${teste.name.toUpperCase()} >> ${teste.evolution.charAt(0).toUpperCase() + (teste.evolution).slice(1)}\n
-// Atributos:\n
-// HP: ${teste.attributes.hp}
-// ATK: ${teste.attributes.attack} SpATK: ${teste.attributes.specialAttack}
-// DEF: ${teste.attributes.defense} SpDEF: ${teste.attributes.specialDefense}
-// SPEED: ${teste.attributes.speed}\n
-// Ataques:
-// Lv 5 -  ${teste.moves[0].name}
-// Lv 9 -  ${teste.moves[3].name}
-// Lv 20 -  ${teste.moves[1].name}
-// Lv 50 -  ${teste.moves[2].name}`)
