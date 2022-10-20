c11e0936fd0d3676c941e99012658ccc8ca0bb72
Guilherme-Soares777 <guilhermesoaresavilars@gmail.com>
2022-10-20 14:44:26 -0300

    iniciando exercício pokemon

--- pokemon.js
+++ pokemon.js
@@ -1,6 +1,10 @@
+const pokemon = require("./db");
 
-const printPokemon = pkm => {
-  console.log(`oie`)
-}
-
-module.exports = { printPokemon }
+const printPokemon = (pkm) => {
+  console.log(`${pkm.name}
+  
+  
+  `);
+};
+printPokemon(pokemon)
+module.exports = { printPokemon };
