ab1d3023d02fe203fa65900341a2b3f7580032ef
Gabriela_gomes3108 <gabrieladepaula8568@gmail.com>
2022-10-20 14:34:56 -0300

    inicio do exercicio

--- pokemon.js
+++ pokemon.js
@@ -1,23 +1,6 @@
 
 const printPokemon = pkm => {
-  console.log(`  Nome: Pikachu - Tipo: Eletrico
-  Habilidade: Static
-
-  Linha de evolução:
-    Pichu >> PIKACHU >> Raichu
-
-  Atributos:
-
-    HP: 100
-    ATK: 55 SpATK: 100
-    DEF: 34 SpDEF: 30
-    SPEED: 150
-
-  Ataques:
-    Lv 5 - Tackle
-    Lv 9 - Thunder Wave
-    Lv 20 - Thunderbolt
-    Lv 50 - Thunder`)
+  console.log(`oie`)
 }
 
 module.exports = { printPokemon }
