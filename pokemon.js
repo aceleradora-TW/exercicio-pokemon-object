ed343d46e80e8b0c1b78489a8c72198a95341c34
Guilherme-Soares777 <guilhermesoaresavilars@gmail.com>
2022-10-21 15:19:47 -0300

    Implemetando função getMoves

--- pokemon.js
+++ pokemon.js
@@ -1,10 +1,23 @@
 const pokemon = require("./db");
 
+const getMoves = (moves) => {
+  let mapeando = moves.map((move) => {
+    `Lv ${move.lv} - ${move.name}`;
+  });
+  return mapeando;
+};
+
 const printPokemon = (pkm) => {
-  console.log(`${pkm.name}
+  console.log(`
+  ${pkm.name} - ${pkm.types[0]} 
+  ${getMoves(pkm.moves)}
   
   
   `);
 };
-printPokemon(pokemon)
+console.log(printPokemon(pokemon));
+
 module.exports = { printPokemon };
+/*let mapeando = pkm.map((value,index)=>{
+  console.log(value)
+   })*/
