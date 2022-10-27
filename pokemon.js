36579b1d60401d9f4eecef10a48e3391aaf18183
Guilherme-Soares777 <guilhermesoaresavilars@gmail.com>
2022-10-27 13:15:42 -0300

    Refatoração utilizando funções

--- pokemon.js
+++ pokemon.js
@@ -1,100 +1,55 @@
+const { preEvolution } = require("./db");
 const pokemon = require("./db");
-let vetorAtacks = [];
-console.log()
 
-const atack = (ataques) => {
-  let mapeando = ataques.moves.map((value, posicao) => {
-    vetorAtacks.push(value.name);
-    vetorAtacks.push(value.lv)
-  });
-
-
-  
-  return mapeando;
+const nameTipo = (nameType) => {
+  console.log(
+    `Nome: ${
+      nameType.name.toUpperCase(1).slice(0, 1) + nameType.name.slice(1)
+    } - Tipo: ${
+      nameType.types[0].toUpperCase(1).slice(0, 1) +
+      nameType.types[0].slice(1).concat("o")
+    }`
+  );
 };
 
-atack(pokemon);
-console.log(vetorAtacks);
-
-const printPokemon = (pkm) => {};
-printPokemon(pokemon);
-
-printPokemon(pokemon);
-
-module.exports = { printPokemon };
-
-/*
- let temp;
-  let mapeando = pkm.moves.map((moves,posicion,arrayInt)=>{
-    temp = moves.lv
-    console.log(arrayInt.lv)
-  })
-  console.log(temp)
-  return mapeando
-
-
-
-
-
-
-
-
-console.log(`
-  Nome: ${pkm.name.toUpperCase(1).slice(0, 1) + pkm.name.slice(1)} - Tipo:${
-    pkm.types[0].toUpperCase(1).slice(0, 1) + pkm.types[0].slice(1).concat("o")
-  }
- Habilidades: ${pkm.ability.toUpperCase(1).slice(0, 1) + pkm.ability.slice(1)}
- 
- Linha de evolução:         
-  ${
-    pkm.preEvolution.toUpperCase(1).slice(0, 1) + pkm.preEvolution.slice(1)
-  } >> ${pkm.name.toUpperCase()} >> ${
-    pkm.evolution.toUpperCase().slice(0, 1) + pkm.evolution.slice(1)
-  }
-
-
-  Atributos:
-
-  hp: ${pkm.attributes.hp}
-  ATK${pkm.attributes.attack} SpATK: ${pkm.attributes.specialAttack}
-  DEF:${pkm.attributes.defense} SpDEFF ${pkm.attributes.specialDefense}
-  SPEED: ${pkm.attributes.speed}
-  
-  Ataques:
-  Lv ${pkm.moves[0].lv} - ${
-    pkm.moves[0].name.toUpperCase(1).slice(0, 1) + pkm.moves[0].name.slice(1)
-  }
-  Lv ${pkm.moves[3].lv} - ${
-    pkm.moves[3].name.toUpperCase(1).slice(0, 1) + pkm.moves[3].name.slice(1)
-  }
-  Lv  ${pkm.moves[1].lv} - ${
-    pkm.moves[1].name.toUpperCase(1).slice(0, 1) + pkm.moves[1].name.slice(1)
-  }
-  Lv  ${pkm.moves[2].lv} - ${
-    pkm.moves[2].name.toUpperCase(1).slice(0, 1) + pkm.moves[2].name.slice(1)
-  }
-  `);*/
-
-/*
-Objt[0].titulo2.push({id2: 'valor'});
-console.log(Objt[0].titulo2);
-
-
-
-
-
-
-
-let mapeando = pkm.map((value,index)=>{
-  console.log(value)
-   })
-   
-   const getMoves = (moves) => {
-  let mapeando = moves.map((move) => {
-    `Lv ${move.lv} - ${move.name}`; 
-
-  });
-  return mapeando;
+const habilidade = (habilidadePkm) => {
+  console.log(
+    `Habilidade: ${
+      habilidadePkm.toUpperCase(1).slice(0, 1) + habilidadePkm.slice(1)
+    }`
+  );
+  console.log("\n");
 };
-   
-   */
+
+const linhaEvolucao = (pokemon) => {
+  console.log(
+    `${
+      pokemon.preEvolution.toUpperCase(1).slice(0, 1) +
+      pokemon.preEvolution.slice(1)
+    } >> ${pokemon.name.toUpperCase(1)} >> ${
+      pokemon.evolution.toUpperCase().slice(0, 1) + pokemon.evolution.slice(1)
+    }`
+  );
+  console.log("\n");
+};
+
+const atributos = (attributes) => {
+  console.log(`HP: ${attributes.hp}
+  ATK: ${attributes.attack} SpATK: ${attributes.specialAttack}
+  DEF: ${attributes.defense} SpDEF: ${attributes.specialDefense}
+ SPEED: ${attributes.speed}
+  `);
+  console.log("\n");
+};
+
+const atack = (moves) => {
+  moves.sort((a, b) => a.lv - b.lv);
+  moves.map((valor) => {
+    console.log(`Lv ${valor.lv} - ${valor.name}`);
+  });
+};
+nameTipo(pokemon);
+habilidade(pokemon.ability);
+linhaEvolucao(pokemon);
+atributos(pokemon.attributes);
+atack(pokemon.moves);
